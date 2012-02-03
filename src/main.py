21deb9e08b2d78d4b6d2678373684a59e0ba1824
Tautvidas <t.9b5as@gmail.com>
2012-02-03 22:56:06 +0200

    Refactoring it into a module

--- src/main.py
+++ src/main.py
@@ -1,7 +1,9 @@
 #Dawkins Weasel program simulation
 import sys
-import random
+import timeit
+import simulator
 
+characters = 'QWERTYUIOPASDFGHJKLZXCVBNM '
 MUTATION_RATE = 0.05
 COPY_LIMIT = 100
 
@@ -10,33 +12,6 @@ if len(target) < 1:
     print 'Input at least 1 character'
     sys.exit()
 
-characters = 'qwertyuiopasdfghjklzxcvbnmQWERTYUIOPASDFGHJKLZXCVBNM '
-
-def scoreIteration(str1, str2):
-    score = 0
-    for key in range(len(str1)):
-        if str1[key] == str2[key]:
-            score += 1
-    return score
-
-def mutate(str, mutation_rate, characters):
-    str = list(str)
-    for key in range(len(str)):
-        if mutation_rate >= random.random():
-            str[key] = characters[random.randint(0, len(characters) - 1)]
-    return ''.join(str)
-
-
-current = ''.join([characters[random.randint(0, len(characters) - 1)] for i in range(len(target))]) 
-iterations = 0
-print '%d: Target "%s", current "%s" (%d of %d)' % (iterations, target, current, scoreIteration(target, current), len(target))
-while current != target:
-    iterations += 1
-    copies = []
-    for i in range(COPY_LIMIT):
-        copies.append(mutate(current, MUTATION_RATE, characters))
-
-    copies = sorted(copies, lambda x, y: cmp(scoreIteration(target, x), scoreIteration(target, y)), reverse=True)    
-    current = copies[0]
-    print '%d: Target "%s", current "%s" (%d of %d)' % (iterations, target, current, scoreIteration(target, current), len(target))
+s = simulator.Simulator(characters, MUTATION_RATE, COPY_LIMIT)
+s.run(target)
 
---
+++ src/simulator.py
@@ -0,0 +1,46 @@
+#Simulator for Dawkins Weasel program
+
+import random
+
+class Simulator:
+    characters = ''
+    mutation_rate = 0.05
+    copy_limit = 100
+    target = ''
+
+    def __init__(self, characters, mutation_rate, copy_limit):
+        self.characters = characters
+        self.mutation_rate = mutation_rate
+        self.copy_limit = copy_limit
+
+    def scoreIteration(self, str):
+        score = 0
+        for key in range(len(self.target)):
+            if self.target[key] == str[key]:
+                score += 1
+        return score
+
+    def mutate(self, str):
+        str = list(str)
+        for key in range(len(str)):
+            if self.mutation_rate >= random.random():
+                str[key] = self.characters[random.randint(0, len(self.characters) - 1)]
+        return ''.join(str)
+
+    def run(self, target):
+        self.target = target
+        current = ''.join([self.characters[random.randint(0, len(self.characters) - 1)] for i in range(len(target))]) 
+        iterations = 0
+        self.printStep(iterations, current)
+        while current != target:
+            iterations += 1
+            copies = []
+            for i in range(self.copy_limit):
+                copies.append(self.mutate(current))
+
+            copies = sorted(copies, lambda x, y: cmp(self.scoreIteration(x), self.scoreIteration(y)), reverse=True)    
+            current = copies[0]
+            self.printStep(iterations, current)
+
+    def printStep(self, iteration, current):
+        print '%d: Target "%s", current "%s" (%d of %d)' % (iteration, self.target, current, self.scoreIteration(current), len(self.target))
